199f9e8102e25ced13d13aef281c38293c48b439
Jayce Spiva <jayce.spiva@gmail.com>
2024-04-17 05:32:35 -0700

    take 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,31 +1,53 @@
-#import the os module
+# import the os module
 import os
 
-#module for reading CSV files
+# module for reading csv files
 import csv
 
-#set path for file
+# setting variables
+months = []
+profit_losses = []
+changes = []
+
+# set path to read csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-#open and read CSV
+# read data from csv file
 with open(csvpath) as csvfile:
-    
-    #CSV reader specifies variable that holds content and delimiter
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-    #read the header row first
-    csv_header = next(csvfile)
 
-    total_months = len(list(csvreader))
-    #net_total = sum(int(row[1]) for row in csvreader)
-    
-    #read each row of data after the header
+    # skip header row
+    header = next(csvfile)
+
+    # start reading from row after header row     
     for row in csvreader:
-        net_total = sum(int(row[1]) for row in csvreader)
-        print(f"{net_total}")
-    
-print("Financial Analysis")
-print("--------------------------------------------")
 
-print(f"Total Months: {total_months}")
-print(f"{net_total}")
+        # add date column to month variable/array
+        months.append(row[0])
+
+        # add profit/losses column to profit_losses variable/array
+        profit_losses.append(int(row[1]))
+
+# calculate the total number of months
+total_months = len(months)
+
+# calculate the net total profit/losses
+net_total = sum(profit_losses)
+
+# calculate average percentage change
+for x in range(1, total_months):
+    change = profit_losses[x] - profit_losses[x-1]
+    changes.append(change)
+
+# calculate average percentage change
+average_change = sum(changes)/len(changes)
+
+# greatest increase in profits
+greatest_increase = max(changes)
+greatest_increase_date = 
+
+#
+
+
+print(f'{total_months}')
+print(f'{net_total}')
