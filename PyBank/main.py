9fcdc3ec4ad2b359e24d8a67e1dc9be725cd615f
Jayce Spiva <jayce.spiva@gmail.com>
2024-04-18 05:12:26 -0700

    upload 3

--- PyBank/main.py
+++ PyBank/main.py
@@ -42,12 +42,32 @@ for x in range(1, total_months):
 # calculate average percentage change
 average_change = sum(changes)/len(changes)
 
+# rounding average change to 2 decimal places
+num = average_change 
+rounded_num = round(average_change, 2)
+
 # greatest increase in profits
 greatest_increase = max(changes)
-greatest_increase_date = 
+greatest_increase_date = months[changes.index(greatest_increase) + 1]
+greatest_decrease = min(changes)
+greatest_decrease_date = months[changes.index(greatest_decrease) + 1]
 
-#
+# print analysis
+print(f'Financial Analysis')
+print("---------------------------------------------------")
+print(f'Total Months: {total_months}')
+print(f'Total: ${net_total}')
+print(f'Average Change: ${rounded_num}')
+print(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})')
+print(f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})')
 
-
-print(f'{total_months}')
-print(f'{net_total}')
+# output analysis to text file
+file = open('analysis.txt', 'w')
+file.write("Financial Analysis" + "\n")
+file.write("-----------------------------------" + "\n")
+file.write("Total Months: " + str(total_months) + "\n")
+file.write("Total: " + "$" + str(net_total) + "\n")
+file.write("Average Change: " + "$" + str(rounded_num) + "\n")
+file.write("Greatest Increase in Profits: " + str(greatest_increase_date) + " " + "$" + str(greatest_increase) + "\n") 
+file.write("Greatest Decrease in Profits: " + str(greatest_decrease_date) + " " + "$" + str(greatest_decrease) + "\n")
+file.close
